d261eb09b8fb8838aac05bbc7ee53219b3b48c0e
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-17 19:02:33 +0600

    parameterize function type

--- dist/function.js
+++ dist/function.js
@@ -4,3 +4,8 @@ var myFunc = function () {
     console.log('hello');
 };
 myFunc();
+// ! parameterize function type
+var myFunc2 = function (a, b) {
+    console.log("hello " + a + " " + b);
+};
+myFunc2('Shonjoy', 'Das');
--- src/function.ts
+++ src/function.ts
@@ -3,3 +3,10 @@ const myFunc: Function = () => {
   console.log('hello');
 };
 myFunc();
+
+// ! parameterize function type
+const myFunc2 = (a: string, b: string) => {
+  console.log(`hello ${a} ${b}`);
+};
+
+myFunc2('Shonjoy', 'Das');
