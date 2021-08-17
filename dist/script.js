db1bd4b1f8a43d64dade64416fc529bf10874fdb
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-17 14:47:08 +0600

    Function, Array & Object

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,19 @@
 "use strict";
 console.log('hello');
+// ! FUNCTION
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(3, 7));
+// ! ARRAY
+var fruits = ['apple', 'banana', 'orange'];
+fruits.push('strawberry');
+console.log(fruits);
+// ! OBJECT
+var person = {
+    name: 'Shonjoy',
+    age: 21,
+    isProgramming: true,
+};
+person.age = 22;
+console.log(person);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,24 @@
 console.log('hello');
+
+// ! FUNCTION
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+console.log(multiply(3, 7));
+
+// ! ARRAY
+let fruits = ['apple', 'banana', 'orange'];
+
+fruits.push('strawberry');
+console.log(fruits);
+
+// ! OBJECT
+let person = {
+  name: 'Shonjoy',
+  age: 21,
+  isProgramming: true,
+};
+
+person.age = 22;
+console.log(person);
