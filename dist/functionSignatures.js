0ca3f1396880f8820293050287f65b20ea92cb56
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-17 20:02:57 +0600

    function signatures

---
+++ dist/functionSignatures.js
@@ -0,0 +1,7 @@
+"use strict";
+// ! function signatures
+var add;
+add = function (a, b) {
+    return a + b;
+};
+console.log(add(3, 7));
---
+++ src/functionSignatures.ts
@@ -0,0 +1,8 @@
+// ! function signatures
+let add: (x: number, y: number) => number;
+
+add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(3, 7));
