62d740624eaaf3b162440bcb2b31847e80ad2f76
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-17 18:27:56 +0600

    Explicit & Union Types

---
+++ dist/types.js
@@ -0,0 +1,14 @@
+"use strict";
+// ! normal variable
+var a;
+a = 'Shonjoy';
+// ! array
+var b = [];
+b.push('Shonjoy', 37);
+// ! object
+var c;
+c = {
+    name: 'Shonjoy',
+    age: 21,
+    programmer: true,
+};
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+// ! normal variable
+let a: string | number;
+
+a = 'Shonjoy';
+
+// ! array
+let b: (string | number)[] = [];
+
+b.push('Shonjoy', 37);
+
+// ! object
+let c: {
+  name: string;
+  age: number;
+  programmer: boolean;
+};
+
+c = {
+  name: 'Shonjoy',
+  age: 21,
+  programmer: true,
+};
