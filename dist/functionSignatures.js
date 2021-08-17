77c282df502d9b7f536cc29442175e9259f7950d
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-17 20:09:47 +0600

    function signatures with if else

--- dist/functionSignatures.js
+++ dist/functionSignatures.js
@@ -5,3 +5,15 @@ add = function (a, b) {
     return a + b;
 };
 console.log(add(3, 7));
+// ! function signatures with if else
+var calculation;
+calculation = function (a, b, c) {
+    if (c === 'add') {
+        return a + b;
+    }
+    else {
+        return a - b;
+    }
+};
+console.log(calculation(3, 7, 'add'));
+console.log(calculation(3, 7, 'minus'));
--- src/functionSignatures.ts
+++ src/functionSignatures.ts
@@ -6,3 +6,17 @@ add = (a, b) => {
 };
 
 console.log(add(3, 7));
+
+// ! function signatures with if else
+let calculation: (x: number, y: number, z: string) => number;
+
+calculation = (a, b, c) => {
+  if (c === 'add') {
+    return a + b;
+  } else {
+    return a - b;
+  }
+};
+
+console.log(calculation(3, 7, 'add'));
+console.log(calculation(3, 7, 'minus'));
