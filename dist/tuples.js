6eb30b978836228e869557f1eefe5c6ffe6eeea0
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-18 20:26:17 +0600

    Tuples

---
+++ dist/tuples.js
@@ -0,0 +1,5 @@
+"use strict";
+let rsArray = [3, 'hello', { rs: 37 }];
+console.log(rsArray);
+let srArray = [7, 'world', { sr: 37 }];
+console.log(srArray);
---
+++ src/tuples.ts
@@ -0,0 +1,5 @@
+let rsArray = [3, 'hello', { rs: 37 }];
+console.log(rsArray);
+
+let srArray: [number, string, object] = [7, 'world', { sr: 37 }];
+console.log(srArray);
