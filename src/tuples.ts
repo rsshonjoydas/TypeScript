d4c213f9423fd35d83842369e4461f5dba648b0c
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-19 03:57:33 +0600

    typle update

--- src/tuples.ts
+++ src/tuples.ts
@@ -3,3 +3,13 @@ console.log(rsArray);
 
 let srArray: [number, string, object] = [7, 'world', { sr: 37 }];
 console.log(srArray);
+
+let employee: [number, string][];
+
+employee = [
+  [1, 'Shonjoy'],
+  [2, 'Joy'],
+  [3, 'Raj'],
+];
+
+console.log(employee);
